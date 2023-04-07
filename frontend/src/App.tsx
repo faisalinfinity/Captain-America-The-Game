f4a02fd5768df224eb865e6398a9f6bfa6019744
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-04-07 20:33:15 +0530

    Changed folder name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chat from './components/Chat';
+import Chat from './Components/Chat';
 
 function App() {
   return (
